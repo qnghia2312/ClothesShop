79b008fa1969e604521152ece062cf7af5edb3e0
qnghia2312 <nguyenquangnghia94.2016@gmail.com>
2024-05-06 16:45:26 +0700

    delete no usage

--- app/src/main/java/com/example/clothes/Cart/CartFragment.java
+++ app/src/main/java/com/example/clothes/Cart/CartFragment.java
@@ -284,39 +284,6 @@ public class CartFragment extends Fragment {
         Toast.makeText(getContext(), "item details at: "+ position, Toast.LENGTH_LONG).show();
     }
 
-
-
-
-    private void thanhToanZaloPay(int price) throws Exception {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        ZaloPaySDK.init(2553, Environment.SANDBOX);
-        try {
-            CreateOrder orderApi = new CreateOrder();
-            JSONObject data = orderApi.createOrder(String.valueOf(price));
-
-            String code=data.getString("return_code");
-            if (code.equals("1")) {
-                String token = data.getString("zp_trans_token");
-                ZaloPaySDK.getInstance().payOrder(requireActivity(), token, "demozpdk://app", new PayOrderListener() {
-                    @Override
-                    public void onPaymentSucceeded(final String transactionId, final String transToken, final String appTransID) {
-
-                    }
-                    @Override
-                    public void onPaymentCanceled(String s, String s1) {
-                    }
-                    @Override
-                    public void onPaymentError(ZaloPayError zaloPayError, String s, String s1) {
-                    }
-                });
-            }
-        }
-        catch (Exception e) {
-        }
-
-    }
-
     private void Decrease(int position){
         DatabaseReference cartRef = FirebaseDatabase.getInstance().getReference("cart");
         Query query = cartRef.orderByChild("user_id").equalTo(user_id);
@@ -411,100 +378,7 @@ public class CartFragment extends Fragment {
         });
     }
 
-    private void PayForAll(AtomicDouble totalCost){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle("Payment Confirmation");
-        builder.setMessage("The total amount is " + totalCost + "đ. Do you want to continue?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                paymentMethodLayout.setVisibility(View.VISIBLE);
-                RadioGroup paymentMethod = paymentMethodLayout.findViewById(R.id.paymentMethod);
-                Button btnConfirmMethod = paymentMethodLayout.findViewById(R.id.buttonConfirmMethod);
-                btnConfirmMethod.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        int selectedMethodId = paymentMethod.getCheckedRadioButtonId();
-                        RadioButton selected = paymentMethodLayout.findViewById(selectedMethodId);
-                        String method = selected.getText().toString();
 
-                        int tt = (int) totalCost.get();
-
-                        if(method.equals("Thanh toán khi nhận hàng")){
-                            Toast.makeText(getActivity(), "Thanh toán khi nhận hàng: " + totalCost, Toast.LENGTH_LONG).show();
-                        }else if(method.equals("Thanh toán ZaloPay")){
-                            //Thanh toán
-                            try {
-                                thanhToanZaloPay(tt);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-
-                        paymentMethodLayout.setVisibility(View.GONE);
-//                        Toast.makeText(getActivity(), method, Toast.LENGTH_LONG).show();
-
-                        DatabaseReference cartRef = FirebaseDatabase.getInstance().getReference("cart");
-                        Query cartQuery = cartRef.orderByChild("user_id").equalTo(user_id);
-
-                        DatabaseReference orderRef = FirebaseDatabase.getInstance().getReference("order");
-                        DatabaseReference orderDetailsRef = FirebaseDatabase.getInstance().getReference("orderDetail");
-
-                        order ord = new order();
-                        String newId = orderRef.push().getKey();
-                        ord.setId(newId);
-                        ord.setStatus(1);
-                        ord.setUser_id(user_id);
-
-                        ord.setTotalPrice(tt);
-                        Date currentdate = new Date();
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-                        String formatted = dateFormat.format(currentdate);
-                        ord.setCreateAt(formatted);
-                        ord.setUpdateAt(formatted);
-                        ord.setPayment(method);
-
-                        orderRef.child(newId).setValue(ord);
-
-
-                        cartQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                for(DataSnapshot cartItem : snapshot.getChildren()){
-                                    Cart cart = cartItem.getValue(Cart.class);
-
-                                    DatabaseReference newOrderDetailRef = orderDetailsRef.child(newId);
-                                    // Thêm dữ liệu cho từng nhánh con
-                                    newOrderDetailRef.child(cart.getProduct_id()).child(cart.getSize()).setValue(cart.getQuantity());
-
-                                    cartItem.getRef().removeValue();
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
-
-                    }
-                });
-
-
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
-
-
-        Toast.makeText(getContext(), "Total cost: " + totalCost, Toast.LENGTH_SHORT).show();
-    }
 
 
     @Override
